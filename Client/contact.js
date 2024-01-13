68bbc064f51e43bf33091d328b052f425683318b
Christener19 <christenerleigh7070@gmail.com>
2024-01-13 19:46:07 +0000

    testing deployed version one last time tongiht

--- Client/contact.js
+++ Client/contact.js
@@ -5,7 +5,9 @@ document.querySelector('form').addEventListener('submit', async (event) => {
     const formData = new FormData(event.target);
     const jsonData = Object.fromEntries(formData.entries());
 
-    const response = await fetch("http://localhost:3000/api/contact", {
+    const response = await fetch("https://christener.vercel.app/api/contact",
+      // "http://localhost:3000/api/contact",
+      {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(jsonData)
