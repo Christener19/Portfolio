f81cc1ab6518c2aa6e3c1ec9bfa008cf7805cb2c
Christener19 <christenerleigh7070@gmail.com>
2024-01-13 18:25:19 +0000

    new try

--- Client/contact.js
+++ Client/contact.js
@@ -1,14 +1,25 @@
-document.querySelector('form').addEventListener('submit', (event) => {
-  event.preventDefault();
-  const formData = new FormData(event.target);
-  const jsonData = Object.fromEntries(formData.entries());
+document.querySelector('form').addEventListener('submit', async (event) => {
+  try {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const jsonData = Object.fromEntries(formData.entries());
 
-  fetch('https://christener.vercel.app/api/contact', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(jsonData)
-  })
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+    const response = await fetch('https://christener.vercel.app/api/contact', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(jsonData)
+    });
+
+    const responseText = await response.text();
+    console.log('Response:', responseText);
+
+    if (response.ok) {
+      const responseData = JSON.parse(responseText);
+      console.log('Parsed Data:', responseData);
+    } else {
+      console.error('Error:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
 });
